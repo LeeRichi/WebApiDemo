36dc0ea82d3c6f7ff9e4058b8c2e404a04db587b
Rich <applerich0306@gmail.com>
2023-07-30 23:56:08 +0300

    clean empty space

--- Dto/UserDto.cs
+++ Dto/UserDto.cs
@@ -5,7 +5,6 @@ namespace WebProject.Dto
         public string? Name { get; set; }
         public string? Email { get; set; }
         public string? Password { get; set; }
-
         public string? address{ get; set; }
     }
 }
