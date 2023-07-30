300af29ad986d39e7b158d9aef5497d0aeb6d270
Rich <applerich0306@gmail.com>
2023-07-30 23:59:07 +0300

    modify ProductDto.cs

--- Dto/ProductDto.cs
+++ Dto/ProductDto.cs
@@ -2,9 +2,10 @@ namespace FS15_WebApiDemo.Dto
 {
     public class ProductDto
     {
-        public string? Name { get; set; }
-        public string? Email { get; set; }
-        public string? Password { get; set; }
-        public string? address{ get; set; }
+        public Guid Id { get; set;}
+        public string? Title { get; set;}
+        public float Price { get; set; }
+        public int Inventory { get; set; }
+        public string? category { get; set;}
     }
 }
