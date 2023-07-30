e0ef6bf2433eac817be7141473b66ac0783940b5
Rich <applerich0306@gmail.com>
2023-07-30 23:56:57 +0300

    add category to product entity

--- Entities/Product.cs
+++ Entities/Product.cs
@@ -1,15 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace WebProject.Entities
 {
     public class Product
     {
         public Guid Id { get; set;}
-        public string Title { get; set;}
+        public string? Title { get; set;}
         public float Price { get; set; }
         public int Inventory { get; set; }
+        public string? category { get; set;}
+
     }
 }
