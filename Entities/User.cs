017bfd7dc78de4839341929cbc00d68855ac9838
Rich <applerich0306@gmail.com>
2023-07-30 23:57:52 +0300

    clean empty space

--- Entities/User.cs
+++ Entities/User.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using AutoMapper;
 using AutoMapper.Configuration.Annotations;
 using WebProject.Dto;
@@ -12,10 +8,13 @@ namespace WebProject.Entities
     public class User
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
         [Ignore]
-        public byte[] Password { get; set; }
+        public byte[]? Password { get; set; }
+
+        public string? address { get; set; }
+
         public DateOnly CreatedAt { get; set; }
         public DateOnly UpdatedAt { get; set; }
     }
